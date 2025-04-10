6cc41aeeda9aa4521359fae03a809b9d7ce5d490
manlikexavy <chibokaxavier@gmail.com>
2025-04-10 11:41:02 +0100

    installed re charts

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import UserCard from "@/components/UserCard";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      <div className="w-full lg:w-2/3">
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      <div className="w-full lg:w-1/3"></div>
+    </div>
+  );
+};
 
-export default page
+export default page;
---
+++ components/UserCard.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import React from "react";
+
+const UserCard = ({ type }: { type: string }) => {
+  return (
+    <div className="rounded-2xl odd:bg-[#CFCEFF] even:bg-[#FAE27C] p-4 min-w-[130px] flex-1 ">
+      <div className="flex justify-between items-center">
+        <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">2024/2025</span>
+        <Image src="/more.png" width={20} height={20} alt="" />
+      </div>
+      <h1 className="text-2xl font-semibold my-4">1,234</h1>
+      <h2 className="text-sm capitalize font-medium text-gray-500">{type}</h2>
+    </div>
+  );
+};
+
+export default UserCard;
