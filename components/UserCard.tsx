a2999af8255895b1c5624f9cc46bb982bb482716
manlikexavy <chibokaxavier@gmail.com>
2025-04-10 12:16:07 +0100

    count chart for male and female complete

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -5,7 +5,9 @@ const UserCard = ({ type }: { type: string }) => {
   return (
     <div className="rounded-2xl odd:bg-[#CFCEFF] even:bg-[#FAE27C] p-4 min-w-[130px] flex-1 ">
       <div className="flex justify-between items-center">
-        <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">2024/2025</span>
+        <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">
+          2024/2025
+        </span>
         <Image src="/more.png" width={20} height={20} alt="" />
       </div>
       <h1 className="text-2xl font-semibold my-4">1,234</h1>
