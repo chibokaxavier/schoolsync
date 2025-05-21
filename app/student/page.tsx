72e554bccee0c71411bf0478f110978bb7bacb32
manlikexavy <chibokaxavier@gmail.com>
2025-05-21 12:39:23 +0100

    react big calendar set up complete

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,4 +1,5 @@
 import Announcement from "@/components/Announcement";
+import BigCalendar from "@/components/BigCalendar";
 import EventCalendar from "@/components/EventCalendar";
 import React from "react";
 
@@ -8,6 +9,7 @@ const page = () => {
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white  p-4 rounded-md">
           <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
         </div>
       </div>
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
