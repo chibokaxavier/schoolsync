41693a6cbcb0f4db66c8dc6b3edebb2988b284fc
manlikexavy <chibokaxavier@gmail.com>
2025-05-21 11:31:04 +0100

    installed react big calender

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="bg-red-300 text-8xl   ">
-   Chiboka Xavier
-    </div>
-  );
+  return <div className="bg-red-300 text-8xl   ">Chiboka Xavier</div>;
 }
--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Announcement from "@/components/Announcement";
+import EventCalendar from "@/components/EventCalendar";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white  p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+        </div>
+      </div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcement />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
