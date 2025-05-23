1c41e830745f46ae5f6cf2fd85a78c28347075f0
manlikexavy <chibokaxavier@gmail.com>
2025-05-23 20:01:00 +0100

    table in progress

---
+++ app/list/teachers/page.tsx
@@ -0,0 +1,25 @@
+import TableSearch from "@/components/TableSearch";
+import Image from "next/image";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      <div className="flex items-center justify-between ">
+        <h1 className="text-lg font-semibold hidden md:block">All Teachers</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 bg-red-200 w-full md:w-auto">
+          <TableSearch />
+          <div>
+            <button>
+              <Image src="/filter.png" width={14} height={14} alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default page;
