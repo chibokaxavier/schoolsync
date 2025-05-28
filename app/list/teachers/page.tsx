7cf60700c3161aa99df90ab4233b821e97f86131
manlikexavy <chibokaxavier@gmail.com>
2025-05-28 15:07:59 +0100

    Search input styling for searchTable complete

--- app/list/teachers/page.tsx
+++ app/list/teachers/page.tsx
@@ -7,9 +7,9 @@ const page = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       <div className="flex items-center justify-between ">
         <h1 className="text-lg font-semibold hidden md:block">All Teachers</h1>
-        <div className="flex flex-col md:flex-row items-center gap-4 bg-red-200 w-full md:w-auto">
+        <div className="flex flex-col md:flex-row items-center gap-4  w-full md:w-auto">
           <TableSearch />
-          <div>
+          <div className="flex items-center gap-4 self-end">
             <button>
               <Image src="/filter.png" width={14} height={14} alt="" />
             </button>
--- components/TableSearch.tsx
+++ components/TableSearch.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React from "react";
 
 const TableSearch = () => {
   return (
-    <div>TableSearch</div>
-  )
-}
+    <div className="w-full md:w-auto flex items-center text-xs gap-2 rounded-full ring-[1.5px] ring-gray-300 px-2 ">
+      <Image src="/search.png" width={14} height={14} alt="" />
+      <input
+        type="text"
+        placeholder="Search..."
+        className="w-[200px] p-2 bg-transparent outline-none"
+      />
+    </div>
+  );
+};
 
-export default TableSearch
+export default TableSearch;
