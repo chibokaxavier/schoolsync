d66a75ce60c5760fdaba48eec5e3297c73fd8c12
manlikexavy <chibokaxavier@gmail.com>
2025-05-28 15:23:31 +0100

    Paginagtion styling complete

--- app/list/teachers/page.tsx
+++ app/list/teachers/page.tsx
@@ -1,3 +1,4 @@
+import Pagination from "@/components/Pagination";
 import TableSearch from "@/components/TableSearch";
 import Image from "next/image";
 import React from "react";
@@ -10,14 +11,22 @@ const page = () => {
         <div className="flex flex-col md:flex-row items-center gap-4  w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow-100 ">
               <Image src="/filter.png" width={14} height={14} alt="" />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow-100 ">
+              <Image src="/sort.png" width={14} height={14} alt="" />
+            </button>{" "}
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow-100 ">
+              <Image src="/plus.png" width={14} height={14} alt="" />
             </button>
           </div>
         </div>
       </div>
       <div></div>
-      <div></div>
+      <div>
+        <Pagination />
+      </div>
     </div>
   );
 };
